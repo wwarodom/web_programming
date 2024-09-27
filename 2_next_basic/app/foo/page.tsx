159997d0c9fff03bc9973f7b169f8faa6bf8cb84
Warodom WERAPUN <wwarodom@gmail.com>
2024-09-27 12:11:50 +0700

    add image link

--- 2_next_basic/app/foo/page.tsx
+++ 2_next_basic/app/foo/page.tsx
@@ -1,9 +1,16 @@
-
-
+import Link from 'next/link'
+import Image from 'next/image'
 export default function Foo() {
   return (
     <div>
         Foo
+        <Link href="/">Back home</Link>
+        <Image 
+            src="https://www.bnn.in.th/_nuxt/img/site-logo.9ca15bd.svg"
+            width={200}
+            height={200}
+            alt="Picture of the computer"
+        />
     </div>
   )
 }
