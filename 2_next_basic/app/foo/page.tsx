a07e65627acdbc1bcacf404c73ca23a0433a2198
Warodom WERAPUN <wwarodom@gmail.com>
2024-09-27 12:31:26 +0700

    image host

--- 2_next_basic/app/foo/page.tsx
+++ 2_next_basic/app/foo/page.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 export default function Foo() {
   return (
     <div>
-        Foo
+        <div className='flex justify-center border-black 
+                        border-2 text-4xl mb-4 p-4' >Foo</div> 
         <Link href="/">Back home</Link>
         <Image 
-            src="https://www.bnn.in.th/_nuxt/img/site-logo.9ca15bd.svg"
+            src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
             width={200}
             height={200}
             alt="Picture of the computer"
