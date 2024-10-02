23a1a12581bb72047e5ca06e73d638a66ee9d7d2
Warodom WERAPUN <wwarodom@gmail.com>
2024-10-02 11:22:46 +0700

    add product detail template

--- 2_next_basic/app/product/page.tsx
+++ 2_next_basic/app/product/page.tsx
@@ -4,31 +4,29 @@ const products = [
     { name: "A", price: 10 },
     { name: "B", price: 20 },
     { name: "C", price: 30 },
+    { name: "C", price: 30 },
+    { name: "C", price: 30 },
+    { name: "C", price: 30 },
+    { name: "C", price: 30 },
 ]
 
-let mynumber = 90;
-mynumber = 1;
+
+// const instruments = [
+//     { 
+//         name: "Fender Guitar",
+//         price: 300,
+//         image_url: "https://...",
+//         like: 20,
+//         is_new: true,
+//     },
+// ]
 
 export default function Product() {
 
-    function changeMyNumber(newnumber: number) {
-        mynumber = newnumber
-    }
-
     return (
         <div className="flex flex-col items-center">
-            <div>My number in Parent = {mynumber}</div>
-            <ShowMyNumber
-                mynumber={mynumber}
-                changeMyNumber={changeMyNumber}
-            />
             <h1>Product</h1>
-            {/* <ul>
-                <ProductItem name="A" price={10} />
-                <ProductItem name="B" price={20} />
-                <ProductItem name="C" price={30} />
-            </ul> */}
-            <ul>
+            <ul className="flex justify-center gap-4 flex-wrap w-full">
                 {products.map((item, index) =>
                     <ProductItem
                         key={index}
@@ -42,23 +40,33 @@ export default function Product() {
     )
 }
 
-function ShowMyNumber(
-    { mynumber, changeMyNumber }:
-        {
-            mynumber: number,
-            changeMyNumber: (newNumber: number) => void
-        }
-
-) {
-    mynumber = 8;
-    changeMyNumber(9)
-    return (<div>My number in Child {mynumber}</div>)
-}
-
 function ProductItem({ name, price }: {
     name: string;
     price: number;
 }
 ) {
-    return (<li>{name} : {price}</li>)
+    return (
+    <a href="#" className="group block overflow-hidden">
+        <div className="relative w-96 h-[150px] sm:h-[220px]">
+            <img
+                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                alt=""
+                className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0"
+            />
+
+            <img
+                src="https://images.unsplash.com/photo-1523381140794-a1eef18a37c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjQ2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+                alt=""
+                className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100"
+            />
+        </div>
+
+        <div className="relative bg-white pt-3 flex justify-between mb-8">
+            <h3 className="text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                {name}
+            </h3>
+
+            <p className="tracking-wide text-gray-900">${price}</p>
+        </div>
+    </a>)
 }
