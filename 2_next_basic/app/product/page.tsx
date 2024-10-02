3190c6e753f79dc91757130c847dbb54b674140c
Warodom WERAPUN <wwarodom@gmail.com>
2024-10-02 09:40:00 +0700

    add Product detial

--- 2_next_basic/app/product/page.tsx
+++ 2_next_basic/app/product/page.tsx
@@ -1,24 +1,38 @@
 import Link from "next/link";
 
+const products = [
+    { name: "A", price: 10 },
+    { name: "B", price: 20 },
+    { name: "C", price: 30 },
+]
 
 export default function Product() {
     return (
         <div className="flex flex-col items-center">
             <h1>Product</h1>
-            <ul>
+            {/* <ul>
                 <ProductItem name="A" price={10} />
                 <ProductItem name="B" price={20} />
-                <ProductItem name="B" price={30} />
+                <ProductItem name="C" price={30} />
+            </ul> */}
+            <ul>
+                {products.map((item, index) =>
+                    <ProductItem
+                        key={index}
+                        name={item.name}
+                        price={item.price} />)
+                }
             </ul>
+
             <Link href="/">Home</Link>
         </div>
     )
 }
 
-function ProductItem({name, price} : { 
+function ProductItem({ name, price }: {
     name: string;
     price: number;
-} 
+}
 ) {
     return (<li>{name} : {price}</li>)
 }
