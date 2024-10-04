acebcb2b8a2e30080a894cacc619bcb936a56819
Warodom WERAPUN <wwarodom@gmail.com>
2024-10-04 12:13:11 +0700

    explain function JS

--- 2_next_basic/app/todo/page.tsx
+++ 2_next_basic/app/todo/page.tsx
@@ -15,28 +15,50 @@ const tasks = [
 
 export default function Todo() {
     // Create 2 state variables called 'name' , 'time'  
-    const [name, setName ] = useState('')
-    const [time, setTime ] = useState(0)
+    const [name, setName] = useState('')
+    const [time, setTime] = useState(0)
+
+    // javascript tradition function
+    function foo1() {
+        return "foo1"
+    }
+
+    // anonynous function
+    const foo2 = function () {
+        return "foo2"
+    }
+    // arrow function
+    const foo3 = () => {
+        return "foo3"
+    }
+    const foo4 = () => "foo4"
+
+    console.log(`${foo1() + foo2() + foo3() + foo4()}`)
+
     return <>
         <h1>Todo</h1>
         <ul>
-            {tasks.map((item) => 
+            {tasks.map((item) =>
                 <li key={item.id}>
-                    {item.name} : {item.time} 
+                    {item.name} : {item.time}
                 </li>)}
         </ul>
         <br />
         <hr />
-    {/* Create 2 input textbox for name & time */}
+        {/* Create 2 input textbox for name & time */}
         <h1>Add task</h1>
         Name: {name} <br />
         Time: {time} <br />
-        <input 
+        <input
             className="border-2 border-black m-1 text-xl"
-            onChange={ (e) => {
-                console.log(e.target.value)
-            } }
-            type="text" 
-         />
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+        />
+        <br />
+        <input
+            className="border-2 border-black m-1 text-xl"
+            onChange={(e) => setTime(+e.target.value)}
+            type="number"
+        />
     </>
 }
