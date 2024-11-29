f05f9a6924808f6950ccbd4369cfc301e1c882b8
Warodom WERAPUN <wwarodom@gmail.com>
2024-11-29 08:37:36 +0700

    add shadcn-ui

---
+++ 4_shadcn_app/app/page.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+      <> 
+        <Button>Add</Button>
+        <Badge>Badge</Badge>
+      </>
+  );
+}
